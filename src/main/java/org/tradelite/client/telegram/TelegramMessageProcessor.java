e66b075421a377986f2704329aea7c205dbad421
Lucas Elsaesser <29556+D071137@users.noreply.github.wdf.sap.corp>
2025-06-03 00:15:17 +0200

    send confirmation message on price update

--- src/main/java/org/tradelite/client/telegram/TelegramMessageProcessor.java
+++ src/main/java/org/tradelite/client/telegram/TelegramMessageProcessor.java
@@ -1,6 +1,7 @@
 package org.tradelite.client.telegram;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.tradelite.client.telegram.dto.TelegramUpdateResponse;
 
@@ -9,6 +10,13 @@ import java.util.List;
 @Slf4j
 @Component
 public class TelegramMessageProcessor {
+
+    private final TelegramClient telegramClient;
+
+    @Autowired
+    public TelegramMessageProcessor(TelegramClient telegramClient) {
+        this.telegramClient = telegramClient;
+    }
 
     public void parseMessage(List<TelegramUpdateResponse> chatUpdates) {
         for (TelegramUpdateResponse update : chatUpdates) {
@@ -24,6 +32,8 @@ public class TelegramMessageProcessor {
                     double target = Double.parseDouble(parts[3]); // e.g. 52
 
                     log.info("Received set message for command: {}, symbol: {}, target: {}", subCommand, symbol, target);
+                    telegramClient.sendMessage("All set!\n" +
+                            "Updated " + subCommand + " price for " + symbol + " to " + target + ".");
                 }
             }
         }
