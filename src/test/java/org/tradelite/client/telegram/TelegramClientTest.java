00694d4e3d8d50eb0af8951c57f7732e13fd73e1
Lucas Elsaesser <29556+D071137@users.noreply.github.wdf.sap.corp>
2025-06-04 21:24:38 +0200

    telegram client tests

--- src/test/java/org/tradelite/client/telegram/TelegramClientTest.java
+++ src/test/java/org/tradelite/client/telegram/TelegramClientTest.java
@@ -10,7 +10,9 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+import org.tradelite.client.telegram.dto.TelegramUpdateResponseWrapper;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 import static org.tradelite.client.telegram.TelegramClient.BASE_URL;
@@ -42,5 +44,67 @@ class TelegramClientTest {
         telegramClient.sendMessage(message);
 
         verify(restTemplate, times(1)).exchange(eq(expectedUrl), eq(HttpMethod.POST), any(HttpEntity.class), eq(String.class));
+    }
+
+    @Test
+    void sendMessage_failure() {
+        String message = "Test message";
+        String expectedUrl = String.format(BASE_URL, "testToken", "testChatId", message);
+        when(restTemplate.exchange(
+                eq(expectedUrl),
+                eq(HttpMethod.POST),
+                any(HttpEntity.class),
+                eq(String.class)
+        )).thenReturn(new ResponseEntity<>("Error", HttpStatus.INTERNAL_SERVER_ERROR));
+
+        telegramClient.sendMessage(message);
+
+        verify(restTemplate, times(1)).exchange(eq(expectedUrl), eq(HttpMethod.POST), any(HttpEntity.class), eq(String.class));
+    }
+
+    @Test
+    void sendMessage_exception() {
+        String message = "Test message";
+        String expectedUrl = String.format(BASE_URL, "testToken", "testChatId", message);
+        when(restTemplate.exchange(
+                eq(expectedUrl),
+                eq(HttpMethod.POST),
+                any(HttpEntity.class),
+                eq(String.class)
+        )).thenThrow(new RuntimeException("Network error"));
+
+        telegramClient.sendMessage(message);
+
+        verify(restTemplate, times(1)).exchange(eq(expectedUrl), eq(HttpMethod.POST), any(HttpEntity.class), eq(String.class));
+    }
+
+    @Test
+    void getChatUpdates_success() {
+        String url = String.format("https://api.telegram.org/bot%s/getUpdates", "testToken");
+        when(restTemplate.exchange(
+                eq(url),
+                eq(HttpMethod.GET),
+                any(HttpEntity.class),
+                eq(TelegramUpdateResponseWrapper.class)
+        )).thenReturn(new ResponseEntity<>(new TelegramUpdateResponseWrapper(), HttpStatus.OK));
+
+        telegramClient.getChatUpdates();
+
+        verify(restTemplate, times(1)).exchange(eq(url), eq(HttpMethod.GET), any(HttpEntity.class), eq(TelegramUpdateResponseWrapper.class));
+    }
+
+    @Test
+    void getChatUpdates_exception() {
+        String url = String.format("https://api.telegram.org/bot%s/getUpdates", "testToken");
+        when(restTemplate.exchange(
+                eq(url),
+                eq(HttpMethod.GET),
+                any(HttpEntity.class),
+                eq(TelegramUpdateResponseWrapper.class)
+        )).thenThrow(new RuntimeException("Network error"));
+
+        assertThrows(IllegalStateException.class, () -> telegramClient.getChatUpdates());
+
+        verify(restTemplate, times(1)).exchange(eq(url), eq(HttpMethod.GET), any(HttpEntity.class), eq(TelegramUpdateResponseWrapper.class));
     }
 }
