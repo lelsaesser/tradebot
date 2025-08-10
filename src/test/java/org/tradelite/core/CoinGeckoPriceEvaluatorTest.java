8b33288290ca812a3b8419845423fd50334c32a3
Lucas Elsaesser <29556+D071137@users.noreply.github.wdf.sap.corp>
2025-08-10 20:30:51 +0200

    add tests

--- src/test/java/org/tradelite/core/CoinGeckoPriceEvaluatorTest.java
+++ src/test/java/org/tradelite/core/CoinGeckoPriceEvaluatorTest.java
@@ -7,6 +7,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.tradelite.client.coingecko.CoinGeckoClient;
 import org.tradelite.client.coingecko.dto.CoinGeckoPriceResponse;
+import org.tradelite.client.telegram.TelegramClient;
 import org.tradelite.common.CoinId;
 import org.tradelite.common.TargetPrice;
 import org.tradelite.common.TargetPriceProvider;
@@ -26,12 +27,14 @@ class CoinGeckoPriceEvaluatorTest {
     private CoinGeckoClient coinGeckoClient;
     @Mock
     private TargetPriceProvider targetPriceProvider;
+    @Mock
+    private TelegramClient telegramClient;
 
     private CoinGeckoPriceEvaluator coinGeckoPriceEvaluator;
 
     @BeforeEach
     void setUp() {
-        coinGeckoPriceEvaluator = spy(new CoinGeckoPriceEvaluator(coinGeckoClient, targetPriceProvider, null));
+        coinGeckoPriceEvaluator = spy(new CoinGeckoPriceEvaluator(coinGeckoClient, targetPriceProvider, telegramClient));
     }
 
     @Test
@@ -86,5 +89,66 @@ class CoinGeckoPriceEvaluatorTest {
         assertThat(coinDataSize, is(1));
         verify(coinGeckoClient, times(1)).getCoinPriceData(CoinId.BITCOIN);
         verify(coinGeckoPriceEvaluator, times(1)).comparePrices(coinData.getCoinId(), coinData.getUsd(), 1000, 2000);
+    }
+
+    @Test
+    void evaluateHighPriceChange_shouldSendMessageWhenPriceSwingExceedsThreshold() {
+        CoinGeckoPriceResponse.CoinData coinData = new CoinGeckoPriceResponse.CoinData();
+        coinData.setCoinId(CoinId.BITCOIN);
+        coinData.setUsd(100.0);
+
+        coinGeckoPriceEvaluator.lastPriceCache.put(CoinId.BITCOIN, 90.0);
+        coinGeckoPriceEvaluator.dailyLowPrice.put(CoinId.BITCOIN, 95.0);
+        coinGeckoPriceEvaluator.dailyHighPrice.put(CoinId.BITCOIN, 105.0);
+
+        when(targetPriceProvider.isSymbolIgnored(CoinId.BITCOIN, IgnoreReason.CHANGE_PERCENT_ALERT)).thenReturn(false);
+
+        coinGeckoPriceEvaluator.evaluateHighPriceChange(coinData);
+
+        verify(telegramClient, times(1)).sendMessage(anyString());
+        verify(targetPriceProvider, times(1)).addIgnoredSymbol(CoinId.BITCOIN, IgnoreReason.CHANGE_PERCENT_ALERT);
+    }
+
+    @Test
+    void evaluateHighPriceChange_shouldNotSendMessageWhenPriceSwingIsBelowThreshold() {
+        CoinGeckoPriceResponse.CoinData coinData = new CoinGeckoPriceResponse.CoinData();
+        coinData.setCoinId(CoinId.BITCOIN);
+        coinData.setUsd(100.0);
+
+        coinGeckoPriceEvaluator.dailyLowPrice.put(CoinId.BITCOIN, 98.0);
+        coinGeckoPriceEvaluator.dailyHighPrice.put(CoinId.BITCOIN, 102.0);
+
+        coinGeckoPriceEvaluator.evaluateHighPriceChange(coinData);
+
+        verify(telegramClient, never()).sendMessage(anyString());
+        verify(targetPriceProvider, never()).addIgnoredSymbol(any(), any());
+    }
+
+    @Test
+    void evaluateHighPriceChange_shouldNotSendMessageWhenSymbolIsIgnored() {
+        CoinGeckoPriceResponse.CoinData coinData = new CoinGeckoPriceResponse.CoinData();
+        coinData.setCoinId(CoinId.BITCOIN);
+        coinData.setUsd(100.0);
+
+        coinGeckoPriceEvaluator.dailyLowPrice.put(CoinId.BITCOIN, 90.0);
+        coinGeckoPriceEvaluator.dailyHighPrice.put(CoinId.BITCOIN, 110.0);
+
+        when(targetPriceProvider.isSymbolIgnored(CoinId.BITCOIN, IgnoreReason.CHANGE_PERCENT_ALERT)).thenReturn(true);
+
+        coinGeckoPriceEvaluator.evaluateHighPriceChange(coinData);
+
+        verify(telegramClient, never()).sendMessage(anyString());
+        verify(targetPriceProvider, never()).addIgnoredSymbol(any(), any());
+    }
+
+    @Test
+    void resetDailyPrices_shouldClearMaps() {
+        coinGeckoPriceEvaluator.dailyLowPrice.put(CoinId.BITCOIN, 100.0);
+        coinGeckoPriceEvaluator.dailyHighPrice.put(CoinId.ETHEREUM, 200.0);
+
+        coinGeckoPriceEvaluator.resetDailyPrices();
+
+        assertThat(coinGeckoPriceEvaluator.dailyLowPrice.isEmpty(), is(true));
+        assertThat(coinGeckoPriceEvaluator.dailyHighPrice.isEmpty(), is(true));
     }
 }
