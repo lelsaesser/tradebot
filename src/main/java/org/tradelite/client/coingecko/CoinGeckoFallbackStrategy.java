dc789b7b7070d8e4a0eb955392d5c098b48d43ed
Moritz Müller <moritz.mueller@posteo.de>
2026-04-03 12:01:05 +0200

    removing fixtures and strategies

--- src/main/java/org/tradelite/client/coingecko/CoinGeckoFallbackStrategy.java
+++
@@ -1,8 +0,0 @@
-package org.tradelite.client.coingecko;
-
-import org.tradelite.client.coingecko.dto.CoinGeckoPriceResponse;
-import org.tradelite.common.CoinId;
-
-public interface CoinGeckoFallbackStrategy {
-    CoinGeckoPriceResponse.CoinData onPriceFailure(CoinId coinId, Exception cause);
-}
