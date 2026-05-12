0486a9d3d33677627b28f2d7870118a3ae4e96aa
Lucas Elsaesser <lucas.elsaesser@gmail.com>
2026-05-12 14:49:30 +0200

    Fix flaky JDBC test context loading under parallel execution

--- src/test/java/org/tradelite/repository/AbstractSqliteRepositoryTest.java
+++ src/test/java/org/tradelite/repository/AbstractSqliteRepositoryTest.java
@@ -1,13 +1,18 @@
 package org.tradelite.repository;
 
 import java.util.UUID;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.JdbcTemplateAutoConfiguration;
 import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 import org.springframework.boot.jdbc.test.autoconfigure.JdbcTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 
 @JdbcTest
+@ContextConfiguration(
+        classes = {DataSourceAutoConfiguration.class, JdbcTemplateAutoConfiguration.class})
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Sql("classpath:schema.sql")
 abstract class AbstractSqliteRepositoryTest {
@@ -16,5 +21,7 @@ abstract class AbstractSqliteRepositoryTest {
     static void configureDatasource(DynamicPropertyRegistry registry) {
         String url = "jdbc:sqlite:file:" + UUID.randomUUID() + "?mode=memory&cache=shared";
         registry.add("spring.datasource.url", () -> url);
+        registry.add("spring.datasource.driver-class-name", () -> "org.sqlite.JDBC");
+        registry.add("spring.datasource.hikari.maximum-pool-size", () -> "1");
     }
 }
