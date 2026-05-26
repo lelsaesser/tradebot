adc1d2f8bca8b45d1139846c5e8428082e93f92e
Lucas Elsaesser <lucas.elsaesser@gmail.com>
2026-05-26 17:46:24 +0200

    Add main TaskScheduler bean

--- src/main/java/org/tradelite/config/SchedulingConfig.java
+++ src/main/java/org/tradelite/config/SchedulingConfig.java
@@ -1,8 +1,11 @@
 package org.tradelite.config;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @Configuration
 @EnableScheduling
@@ -10,4 +13,13 @@ import org.springframework.scheduling.annotation.EnableScheduling;
         name = "tradebot.scheduling.enabled",
         havingValue = "true",
         matchIfMissing = true)
-public class SchedulingConfig {}
+public class SchedulingConfig {
+
+    @Bean
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(1);
+        scheduler.setThreadNamePrefix("scheduling-");
+        return scheduler;
+    }
+}
